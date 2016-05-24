ef76e98b690e29e0f6bf97bcad04f9e0580e8d54
Marcus Maczynski <keldren@gmail.com>
2016-05-24 00:26:27 -0700

    Mid progress.  Terrible time to push.

--- TemplateScrubber/TemplateScrubber/MainWindow.xaml.cs
+++ TemplateScrubber/TemplateScrubber/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Text.RegularExpressions;
 using System.IO;
+using WinForms = System.Windows.Forms;
 
 namespace TemplateScrubber
 {
@@ -68,32 +69,65 @@ namespace TemplateScrubber
             // Let's create a directory for us to work in.
             if(filesToScrub.Count() != 0)
             {
-                SaveFileDialog saveScrubbedTemplateDialog = new SaveFileDialog();
-                saveScrubbedTemplateDialog.InitialDirectory = filesToScrub[0].FilePath;
+                string saveFolderPath;
 
-                Nullable<bool> result = saveScrubbedTemplateDialog.ShowDialog();
+                WinForms.FolderBrowserDialog saveScrubbedTemplateDialog = new System.Windows.Forms.FolderBrowserDialog();
+                saveScrubbedTemplateDialog.RootFolder = Environment.SpecialFolder.MyDocuments;
 
-                Regex mountFinder = new Regex(mountRemovalPattern);
-                string input = "";
-                foreach (FileToScrub file in filesToScrub)
+                saveScrubbedTemplateDialog.Description = "Select a directory for storing scrubbed templates.";
+                WinForms.DialogResult result = saveScrubbedTemplateDialog.ShowDialog();
+
+                if(result == WinForms.DialogResult.OK)
                 {
+                    saveFolderPath = saveScrubbedTemplateDialog.SelectedPath;
 
-                    StreamReader fileReader = new StreamReader(file.FilePath);
-                    while (input != null)
+                    string modifiedSaveFolderPath = saveFolderPath + "\\ScrubbedTemplates-" + DateTime.Now.ToString("yyyMMdd-HH.mm.ss");
+                    Directory.CreateDirectory(modifiedSaveFolderPath);
+
+                    Regex mountFinder = new Regex(mountRemovalPattern);
+                    string input = "";
+                    // Debut step. TODO: Remove
+                    int linesReadCount = 0;
+                    int linesWroteCount = 0;
+                    // -----------
+
+                    foreach (FileToScrub file in filesToScrub)
                     {
-                        input = fileReader.ReadLine();
 
-                        if (input != null)
+                        StreamReader fileReader = new StreamReader(file.FilePath);
+                        StreamWriter fileWriter = new StreamWriter(modifiedSaveFolderPath + "\\" + file.FileName.ToString());
+                        fileWriter.
+                        fileWriter.Flush();
+                        while (!fileReader.EndOfStream)
                         {
-                            bool matchFound = false;
+                            input = fileReader.ReadLine();
+                            linesReadCount++;
 
-                            if (!matchFound)
+                            if (!fileReader.EndOfStream)
                             {
+                                bool matchFound = false;
 
+                                if (!matchFound)
+                                {
+                                    fileWriter.WriteLine(input);
+                                    linesWroteCount++;
+                                }
                             }
                         }
+                        fileWriter.Close();
+                        fileReader.Close();
                     }
+                    
+                    // Debug step. TODO: remove.
+                    MessageBox.Show("Read " + linesReadCount.ToString() + " lines.  Wrote " + linesWroteCount.ToString() + " lines.", "Done");
+
                 }
+                else
+                {
+                    MessageBox.Show("Invalid Save Folder selected.", "Error - Invalid Save Spot");
+                }
+
+                
             }
             else
             {
